699e86c96b5653fe264f8fefb18c11b005c7a93d
Damir Aubekerov <80846714+Yaksat@users.noreply.github.com>
2023-02-26 11:56:17 +0400

    show post

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -24,10 +24,10 @@ class BlogController extends Controller
     //   $posts = Post::query()->limit(12)->get();
     //   $posts = Post::query()->limit(12)->offset(12)->get();
 
-        $validated = $request->validate([
-           'limit' => ['nullable', 'integer', 'min:1', 'max:100'],
-           'page' => ['nullable', 'integer', 'min:1', 'max:100'],
-        ]);
+//        $validated = $request->validate([
+//           'limit' => ['nullable', 'integer', 'min:1', 'max:100'],
+//           'page' => ['nullable', 'integer', 'min:1', 'max:100'],
+//        ]);
 
 //        $page = $validated['page'] ?? 1;
 //        $limit = $validated['limit'] ?? 12;
@@ -43,16 +43,34 @@ class BlogController extends Controller
 
     }
 
-    public function show ($post)
+    public function show (Request $request, Post $post)
     {
         //Это пример того как можно проверять по какому роуту пришел запрос
         // return Route::is('blog*') ? 'yes' : 'no';
 
-        $post = (object) [
-            'id' => 123,
-            'title' => 'Lorem ipsum dolor sit amet.',
-            'content' => 'Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipisicing elit. Delectus, nemo?'
-        ];
+//        $post = Post::query()
+//            ->latest('published_at')
+//            ->firstOrFail(['id', 'title']);
+
+        //$post = Post::query()->findOrFail($post);
+        //$posts = Post::query()->find([1, 2, 3]);
+        // Это все для того случая,когда явно не указан тип у post public function show (Request $request, $post)
+        // Если явно указано Post $post, то можно сразу view возвращать.
+
+
+     // Чтобы использовать кэширование передавать нужно без указания типа Post
+        // public function show (Request $request, $post)
+
+//        $post = cache()->remember(
+//            key: "posts.{$post}",
+//            ttl: now()->addHour(),
+//            callback: function () use ($post) {
+//                return Post::query()->findOrFail($post);
+//            }
+//        );
+        // Если есть по данному ключу данные, то берем из кэша, если нет то с попощью коллбэк функции получаем из БД и
+        // одновременно записываем в кэш.
+        // Способ с кэшем используем, если большая нагрузка на сайт
 
         return view('blog.show', compact('post'));
     }
